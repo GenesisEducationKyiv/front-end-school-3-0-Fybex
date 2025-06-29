5a23860a27ef655d3520b60570b8a7254b73ba3f
Andrii Valenia <a.valenia@newtonideas.com>
2025-06-29 18:36:59 +0300

    chore: remove proto dist from git

--- packages/proto/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './generated/genres/v1/genres_pb.js';
-export * from './generated/tracks/v1/tracks_pb.js';
-export * from './generated/health/v1/health_pb.js';
-export { create } from '@bufbuild/protobuf';
-export type { Message } from '@bufbuild/protobuf';
